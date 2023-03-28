3a18c3434f487bb8acb9733542e576d64d2b6277
setodeve <keinick11@outlook.com>
2023-03-28 20:57:05 +0900

    Edit react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
