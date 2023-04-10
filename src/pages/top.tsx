5a6c5664de72ecf75699652c3d4198aa520fee3d
setodeve <keinick11@outlook.com>
2023-04-10 20:48:19 +0900

    Edit top page

--- src/pages/top.tsx
+++ src/pages/top.tsx
@@ -1,1 +1,24 @@
+import React, { useContext } from "react"
 import { TodoInfoContext } from '../data/data'
+
+interface Todo{
+  id:number,
+  title:string,
+  category:string,
+  priority:string,
+  time:number,
+  like:string,
+  status:string
+}
+
+interface TodoList{
+  todos: Array<Todo>
+}
+
+export const Top : React.FC<TodoList> = (props) => {
+  return (
+    <div>
+
+    </div>
+  )
+}
