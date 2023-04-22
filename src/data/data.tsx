8caa26ba1dcb5577b64ab91001a72198c005493a
setodeve <keinick11@outlook.com>
2023-04-22 20:59:55 +0900

    Edit GetTodoData

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -12,7 +12,14 @@ interface Todo{
 
 export const TodoInfoContext = createContext({});
 export const TodoInfoProvider = (props:any) => {
-  const [todoInfo,setTodoInfo] = useState({todos:[]});
+  const [todoInfo,setTodoInfo] = useState({todos:[
+    {id:1, title:"Call Customer", category:"work", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:2, title:"Write out agenda", category:"work", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:3, title:"Go Swimming", category:"play", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:4, title:"Walk around", category:"play", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:5, title:"Go Hospital", category:"errand", priority:"2", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:6, title:"Go City Center", category:"errand", priority:"2", time:new Date("2024-03-25"), like:true, status:"done"},
+  ]});
   return(
     <TodoInfoContext.Provider value={{todoInfo,setTodoInfo}} >
       {props.children}
@@ -20,14 +27,14 @@ export const TodoInfoProvider = (props:any) => {
   )
 }
 
-export const GetTodos = () => {
-  const todos : Todo[] = [
-    {id:1, title:"Call Customer", category:"work", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
-    {id:2, title:"Write out agenda", category:"work", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
-    {id:3, title:"Go Swimming", category:"play", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
-    {id:4, title:"Walk around", category:"play", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
-    {id:5, title:"Go Hospital", category:"errand", priority:"2", time:new Date("2024-03-25"), like:true, status:"done"},
-    {id:6, title:"Go City Center", category:"errand", priority:"2", time:new Date("2024-03-25"), like:true, status:"done"},
-  ]
-  return todos
-}
+// export const GetTodos = () => {
+//   const todos : Todo[] = [
+//     {id:1, title:"Call Customer", category:"work", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+//     {id:2, title:"Write out agenda", category:"work", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+//     {id:3, title:"Go Swimming", category:"play", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+//     {id:4, title:"Walk around", category:"play", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+//     {id:5, title:"Go Hospital", category:"errand", priority:"2", time:new Date("2024-03-25"), like:true, status:"done"},
+//     {id:6, title:"Go City Center", category:"errand", priority:"2", time:new Date("2024-03-25"), like:true, status:"done"},
+//   ]
+//   return todos
+// }
