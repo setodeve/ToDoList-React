1238221ed3dc662a9fd6bbe6fe59b0eab69e169a
setodeve <keinick11@outlook.com>
2023-04-22 19:44:07 +0900

    Add GetTodoData

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -5,8 +5,8 @@ interface Todo{
   title:string,
   category:string,
   priority:string,
-  time:number,
-  like:string,
+  time:Date,
+  like:boolean,
   status:string
 }
 
@@ -18,4 +18,16 @@ export const TodoInfoProvider = (props:any) => {
       {props.children}
     </TodoInfoContext.Provider>
   )
+}
+
+export const GetTodos = () => {
+  const todos : Todo[] = [
+    {id:1, title:"Call Customer", category:"work", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:2, title:"Write out agenda", category:"work", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:3, title:"Go Swimming", category:"play", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:4, title:"Walk around", category:"play", priority:"1", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:5, title:"Go Hospital", category:"errand", priority:"2", time:new Date("2024-03-25"), like:true, status:"done"},
+    {id:6, title:"Go City Center", category:"errand", priority:"2", time:new Date("2024-03-25"), like:true, status:"done"},
+  ]
+  return todos
 }
