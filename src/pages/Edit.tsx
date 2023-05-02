46c057cd42d3f44a1e9c04033cf0ed269965f468
setodeve <keinick11@outlook.com>
2023-05-02 22:28:36 +0900

    Edit edit component

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react"
 import { TodoInfoContext } from '../data/data'
-import { useParams } from "react-router-dom"
+import { Form,useParams } from "react-router-dom"
 
 interface Todo{
   id:number,
@@ -12,22 +12,41 @@ interface Todo{
   status:string
 }
 
-const findTodo = () => {
-  const paramsData  = useParams();
-  const {todoInfo,setTodoInfo} = useContext(TodoInfoContext)
-  return todoInfo.filter((todo:Todo)=>todo.id==Number(paramsData.todoID))
-}
-
-const chageTodo = () => {
-  
-}
-
 export const Edit = () => {
+  const {todoInfo,setTodoInfo} = useContext(TodoInfoContext)
+  const paramsData  = useParams();
+
+  const findTodo = () => {
+    return todoInfo.filter((todo:Todo)=>todo.id==Number(paramsData.todoID))
+  }
+  
+  const chageTodo = (e) => {
+    const todos = [...todoInfo]
+      
+    todos.map((todo) =>{
+      if(todo.id==Number(paramsData.todoID)){
+        todo.title = e.target.value
+      }else{
+        //IDに一致しないものは何もしない
+      }
+      return todo
+    })
+    setTodoInfo(todos)
+  }
+  
+  const afterSubmit = (e) => {
+    console.log(todoInfo)
+  }
+
   const todo = findTodo()
   return (
-    <div>
-      edit
-    </div>
+    <Form method="get" action="/" onSubmit={afterSubmit}>
+      <label>
+        Title: <input name="myInput" defaultValue={todo[0].title} onChange={chageTodo} />
+      </label>
+      <hr />
+    <button type="submit">Edit</button>
+  </Form>
   )
 }
 
