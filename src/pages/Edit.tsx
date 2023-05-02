0896a494dbcb58d6454558de58b74e7c627ec516
setodeve <keinick11@outlook.com>
2023-05-02 21:08:52 +0900

    Edit edit component

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,4 +1,29 @@
+import React, { useContext } from "react"
+import { TodoInfoContext } from '../data/data'
+import { useParams } from "react-router-dom"
+
+interface Todo{
+  id:number,
+  title:string,
+  category:string,
+  priority:string,
+  time:number,
+  like:string,
+  status:string
+}
+
+const findTodo = () => {
+  const paramsData  = useParams();
+  const {todoInfo,setTodoInfo} = useContext(TodoInfoContext)
+  return todoInfo.filter((todo:Todo)=>todo.id==Number(paramsData.todoID))
+}
+
+const chageTodo = () => {
+  
+}
+
 export const Edit = () => {
+  const todo = findTodo()
   return (
     <div>
       edit
